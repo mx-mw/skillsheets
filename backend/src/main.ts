b3efde68ae1e2c963735a392bdf7017e6bea8fe9
mx-mw <87119201+mx-mw@users.noreply.github.com>
2022-08-16 16:51:24 +1000

    eslint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,3 @@
-import { config } from 'dotenv';
 import fastify, { FastifyRequest } from 'fastify';
 import mongoose from 'mongoose';
 import { Sheet } from './models';
@@ -6,19 +5,17 @@ const port = 8080;
 
 const app = fastify();
 mongoose.connect(process.env.MONGO_URI ?? '')
-	.then(() => console.log("Connected to mongo"))
-	.catch((e) => console.error(e))
+	.then(() => console.log('Connected to mongo'))
+	.catch((e) => console.error(e));
 
-app.get('/:id', async (req: FastifyRequest<{
-	Params: { id: string }
-}>, res) => {
-	let sheet = await Sheet.findById(req.params.id);
-	if (!sheet) 
-		return res.code(400)
-	
-	res.send(sheet)
-})
+app.get('/:id', async (req: FastifyRequest<{ Params: { id: string } }>, res) => {
+	const sheet = await Sheet.findById(req.params.id);
+	if (!sheet)
+		return res.code(400);
+
+	res.send(sheet);
+});
 
 app.listen({
 	port: port,
-}).then(() => console.log(port))
+}).then(() => console.log(port));
